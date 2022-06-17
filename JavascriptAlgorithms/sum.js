ecf2ce9febb3cb584b68deb183a872b56f07c2e6
Abhilash Bhosle <87384608+abhilashbhosle@users.noreply.github.com>
2022-06-17 16:04:20 +0530

    Add files via upload

---
+++ JavascriptAlgorithms/sum.js
@@ -0,0 +1,14 @@
+function sum(n) {
+    let sum = 0
+    for (i = 0; i <= n; i++) {
+        sum += i
+    }
+    console.log(sum)
+}
+sum(4)
+function sum1(n) {
+    console.log(n * (n + 1) / 2)
+}
+sum(4)
+
+
